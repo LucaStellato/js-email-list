63d9135c73db115cb11ed95b1109eae18c8c2cc2
LucaStellato <lucastellato435@gmail.com>
2025-10-23 14:18:16 +0200

    complete visualization mail to console

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 console.log('it work')
-
+let listaEl = document.getElementById('lista')
 for(let i=0; i<10; i++){
     axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
 .then((res)=>{
-    console.log(res.data.response)
+   console.log(res.data.response)
+    const mail= res.data.response
+
+
+
 
 })
 }
